84f208ac3f4d11d32c77c55c19b157270f99eaad
Daniel Munoz <de.munoz@gmail.com>
2021-10-16 10:00:16 -0300

    main

--- app/controllers/personas_controller.rb
+++ app/controllers/personas_controller.rb
@@ -1,2 +1,22 @@
 class PersonasController < ApplicationController
+    def index
+        @persona = Persona.new
+      end
+    
+      def create
+        @persona = Persona.create(rut: params[:persona][:rut], apellido_pat: params[:persona][:apellido_pat], apellido_mat: params[:persona][:apellido_mat], nombre: params[:persona][:nombre], tipo: params[:persona][:tipo], direccion: params[:persona][:direccion], fecha_nac: params[:persona][:fecha_nac], n_educacional: params[:persona][:n_educacional], idioma: params[:persona][:idioma])
+        render json: @persona
+      
+      end
+    
+      def show
+        @persona = Persona.find(params[:id])
+      
+      end
+    
+      def showall
+        @persona = Persona.all
+    
+      end
+
 end

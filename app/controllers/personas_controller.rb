0941db65cd7733c5ece8620fed45d4743f8e9843
Daniel Munoz <de.munoz@gmail.com>
2021-10-16 10:15:07 -0300

    main

--- app/controllers/personas_controller.rb
+++ app/controllers/personas_controller.rb
@@ -4,7 +4,7 @@ class PersonasController < ApplicationController
       end
     
       def create
-        @persona = Persona.create(rut: params[:persona][:rut], apellido_pat: params[:persona][:apellido_pat], apellido_mat: params[:persona][:apellido_mat], nombre: params[:persona][:nombre], tipo: params[:persona][:tipo], direccion: params[:persona][:direccion], fecha_nac: params[:persona][:fecha_nac], n_educacional: params[:persona][:n_educaciona], idioma: params[:persona][:idioma])
+        @persona = Persona.create(rut: params[:persona][:rut], apellido_pat: params[:persona][:apellido_pat], apellido_mat: params[:persona][:apellido_mat], nombre: params[:persona][:nombre], tipo: params[:persona][:tipo], direccion: params[:persona][:direccion], fecha_nac: params[:persona][:fecha_nac], n_educaciona: params[:persona][:n_educaciona], idioma: params[:persona][:idioma])
         render json: @persona
       
       end
